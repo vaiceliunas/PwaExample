737635ae5bd6cfe2fcef16d5418174aa55f5241f
arnas.vaiceliunas <arnas.vaiceliunas@gmail.com>
2021-08-12 14:56:40 +0300

    Add project files.

---
+++ PwaExample/wwwroot/pwa-installer1.0.js
@@ -0,0 +1,56 @@
+﻿
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
+
+if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('../service-worker1.0.js', { scope: "/" })
+        .then(function (reg) {
+            console.log("Yes, it did.");
+        }).catch(function (err) {
+            console.log("No it didn't. This happened:", err)
+        });
+}
+
+
+if (window.location.protocol === 'http:') {
+    const requireHTTPS = document.getElementById('requireHTTPS');
+    const link = requireHTTPS.querySelector('a');
+    //link.href = window.location.href.replace('http://', 'https://');
+    requireHTTPS.classList.remove('hidden');
+}
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
+    //nesiulyti iskart, tik spaudziant mygtuka
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container
+    divInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
+});
